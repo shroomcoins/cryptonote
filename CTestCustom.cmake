abba5cb339ce69706f2da1fba829bfba1dec62e6
Vinay <vinaysudhir257@gmail.com>
2017-04-04 00:35:10 +0530

    trying build

--- CTestCustom.cmake
+++ CTestCustom.cmake
@@ -8,4 +8,6 @@ set(CTEST_CUSTOM_TESTS_IGNORE
   PerformanceTests
   TransfersTests
   )
-
+  set(CN_PROJECT_NAME "shroom")
+  set(CN_CURRENCY_DISPLAY_NAME "shroom")
+  set(CN_CURRENCY_DISPLAY_NAME "shroom")
